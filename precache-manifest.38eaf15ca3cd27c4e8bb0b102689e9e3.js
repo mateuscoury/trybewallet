5c561484a35d85752597e0711180cb1e2f9a7f51
mateuscoury <mateuscoury@gmail.com>
2021-03-22 20:41:01 -0300

    Updates

---
+++ precache-manifest.38eaf15ca3cd27c4e8bb0b102689e9e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99039346ca799c3edac9f6af92776a2",
+    "url": "/app-toot-name/index.html"
+  },
+  {
+    "revision": "6fa22b8df772fee01ca7",
+    "url": "/app-toot-name/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "913abb9d2160455d76e7",
+    "url": "/app-toot-name/static/js/2.ad1ffaba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/app-toot-name/static/js/2.ad1ffaba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fa22b8df772fee01ca7",
+    "url": "/app-toot-name/static/js/main.f5146c78.chunk.js"
+  },
+  {
+    "revision": "66ac581fbc8990623b1d",
+    "url": "/app-toot-name/static/js/runtime-main.cfc30c8d.js"
+  }
+]);
